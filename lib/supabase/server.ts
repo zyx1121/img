01131c2d6d941e7be5dc775b75566387873acc30
Loki <yongxiang.zhan@outlook.com>
2025-12-29 12:25:38 +0800

    Add Supabase client files and update .gitignore
    - Added new Supabase client files: client.ts, proxy.ts, and server.ts for handling browser and server interactions.
    - Updated .gitignore to remove the Supabase directory exclusion.

---
+++ lib/supabase/server.ts
@@ -0,0 +1,30 @@
+import { createServerClient } from "@supabase/ssr";
+import { type ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+export const createClient = (cookieStore: ReadonlyRequestCookies) => {
+  return createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+};
